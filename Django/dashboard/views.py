311e4ccb1f772674d09796ba19039faf64ba30d6
xVoidDevilx <silasrodriguez02@gmail.com>
2023-10-18 13:53:54 -0500

    Servo to keys

--- Django/dashboard/views.py
+++ Django/dashboard/views.py
@@ -17,8 +17,6 @@ GPIO 13: Fan on Sensehat
 global sense
 sense = SenseHat()
 
-global holding
-
 # For the servos
 import time
 from adafruit_servokit import ServoKit
@@ -93,12 +91,10 @@ def captureKeyEvent(request):
 
         # Check the key and control the GPIO pin accordingly for both keydown and keyup events
         if event == "keydown":
-            holding = True
             if key == 'w' or key == 'ArrowUp':
                 pixels = [colors['red'] if pixel == 1 else colors['white'] for pixel in arrows['up']]
                 sense.set_pixels(pixels)
-                if holding:
-                    pcaScenario()
+                pcaScenario()
                 #Rpi.gpio_control.set_pin_state(Rpi.gpio_control.uppin, GPIO.HIGH)  # Turn on the GPIO pin
             elif key == 's' or key == 'ArrowDown':
                 pixels = [colors['red'] if pixel == 1 else colors['white'] for pixel in arrows['down']]
@@ -116,7 +112,6 @@ def captureKeyEvent(request):
         #key release event
         elif event == "keyup":
             sense.clear()   #clear the sensehat matrix
-            holding = False
             if key == 'w' or key == 'ArrowUp':
                 pcaClear()
                 #Rpi.gpio_control.set_pin_state(Rpi.gpio_control.uppin, GPIO.LOW)  # Turn off the GPIO pin
