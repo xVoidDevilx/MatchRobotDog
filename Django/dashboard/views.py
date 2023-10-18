7b08e070ac9499af9a9cbff153544cfbc070c008
xVoidDevilx <silasrodriguez02@gmail.com>
2023-10-18 13:36:43 -0500

    servo

--- Django/dashboard/views.py
+++ Django/dashboard/views.py
@@ -1,9 +1,8 @@
-import time
-from adafruit_servokit import ServoKit
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+import RPi.GPIO as GPIO
+from sense_hat import SenseHat
 from .arrows import colors, arrows
-
 """
 GPIO 2 & 3 : Sensehat SDA SCL
 GPIO 4: GPCLK0 - Sensehat LED Matrix
@@ -13,14 +12,6 @@ GPIO 27: Pressure and temp sensor on sensehat
 GPIO 5 & 6: Orientation and accelerometer
 GPIO 13: Fan on Sensehat
 """
-
-# Initialize the PCA9685 and create a servo kit object
-kit = ServoKit(channels=16)
-
-# Set the PWM frequency to 50 Hz
-kit.servo[0].set_pulse_width_range(500, 2500)  # Servo 0
-kit.servo[1].set_pulse_width_range(500, 2500)  # Servo 1
-
 
 # init a sense hat object for our robot control HMI
 global sense
@@ -65,28 +56,35 @@ def captureKeyEvent(request):
             if key == 'w' or key == 'ArrowUp':
                 pixels = [colors['red'] if pixel == 1 else colors['white'] for pixel in arrows['up']]
                 sense.set_pixels(pixels)
-                
+                #Rpi.gpio_control.set_pin_state(Rpi.gpio_control.uppin, GPIO.HIGH)  # Turn on the GPIO pin
             elif key == 's' or key == 'ArrowDown':
                 pixels = [colors['red'] if pixel == 1 else colors['white'] for pixel in arrows['down']]
                 sense.set_pixels(pixels)
+                #Rpi.gpio_control.set_pin_state(Rpi.gpio_control.downpin, GPIO.HIGH)
             elif key == 'a' or key == 'ArrowLeft':
                 pixels = [colors['red'] if pixel == 1 else colors['white'] for pixel in arrows['left']]
                 sense.set_pixels(pixels)
+                #Rpi.gpio_control.set_pin_state(Rpi.gpio_control.leftpin, GPIO.HIGH)
             elif key == 'd' or key == 'ArrowRight':
                 pixels = [colors['red'] if pixel == 1 else colors['white'] for pixel in arrows['right']]
                 sense.set_pixels(pixels)
+                #Rpi.gpio_control.set_pin_state(Rpi.gpio_control.rightpin, GPIO.HIGH)
         
         #key release event
         elif event == "keyup":
             sense.clear()   #clear the sensehat matrix
             if key == 'w' or key == 'ArrowUp':
                 pass
+                #Rpi.gpio_control.set_pin_state(Rpi.gpio_control.uppin, GPIO.LOW)  # Turn off the GPIO pin
             elif key == 's' or key == 'ArrowDown':
                 pass
+                ##Rpi.gpio_control.set_pin_state(Rpi.gpio_control.downpin, GPIO.LOW)
             elif key == 'a' or key == 'ArrowLeft':
                 pass
+                #Rpi.gpio_control.set_pin_state(Rpi.gpio_control.leftpin, GPIO.LOW)
             elif key == 'd' or key == 'ArrowRight':
                 pass
+                #Rpi.gpio_control.set_pin_state(Rpi.gpio_control.rightpin, GPIO.LOW)
 
         return JsonResponse({'message': 'Key event captured',
                              'key': key,
